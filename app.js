f03a716e4da2d675e4ffaf9b98fefca191592527
hunseop2772 <gnstjq2772@naver.com>
2022-11-06 16:44:22 +0900

    [HTML, JS] Input Values, Form Submission

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+    const value = loginInput.value;
+    console.log(username)
+}
+
+loginButton.addEventListener("click",onLoginBtnClick);
