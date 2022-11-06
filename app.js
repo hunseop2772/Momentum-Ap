351f0c73717ab7eb941e526ec4b86c797b67cb07
hunseop2772 <gnstjq2772@naver.com>
2022-11-06 18:35:37 +0900

    [js] Saving Username

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 
 const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
+const greeting =document.querySelector("#greeting");
 
-function onLoginBtnClick() {
-    const value = loginInput.value;
-    console.log(username)
+function onLoginBtnClick(event) {
+    event,preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem("username",username);
+    greeting.innerText =`Hello Q${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginButton.addEventListener("click",onLoginBtnClick);
