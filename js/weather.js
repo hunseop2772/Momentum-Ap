1e5f92a4a0602ef4075088f97c48606d8f217614
hunseop2772 <gnstjq2772@naver.com>
2022-11-09 22:19:57 +0900

    [JS, CSS] UPDATE

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,11 @@
 const weather = document.querySelector("#weather span:first-child");
 const city = document.querySelector("#weather span:last-child");
-const API_KEY = "241051bf13976dd3ddf8b8d9f247255e";
+const API_KEY = "3105a6f9caaf25e24c9f732e942e0657";
 
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric&lang=kr`;
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
